6cfdf7c64395105e90b7cd309e802f3dabfca80d
ibrahimBOU <85347626+ibrahimBOU@users.noreply.github.com>
2022-10-12 14:30:56 +0200

    Show one random flag

--- src/components/MyQuiz.vue
+++ src/components/MyQuiz.vue
@@ -1,7 +1,10 @@
 <template >
     <div class="MyQuiz">
+        <div v-if="randompays != null">
+            <img :src="randompays.flags.png" alt="">
+        </div>
         <div :key="index" v-for="(pays, index) in pays">
-            <img :src="pays.flags.png" alt="">
+            <h1>{{ pays.translations.fra }}</h1>
         </div>
     </div>
 </template>
@@ -12,21 +15,28 @@ export default {
     name:"MyQuiz",
     data() {
         return {
-         pays : null   
+         pays : null ,
+         randompays : null,
         }
     },
     mounted() {
        this.getCountries()
     },
     methods: {
-        getCountries() {
-            axios
-            .get(`https://restcountries.com/v3.1/all`)
-            .then((response) => {
-                this.pays = response.data;
-                console.log(this.pays)
-            });
-        }
-},
+            getCountries() {
+                axios
+                .get(`https://restcountries.com/v3.1/all`)
+                .then((response) => {
+                    this.pays = response.data;
+                    this.getRandomCountry()
+                    
+                });
+            },
+            getRandomCountry(){
+                let randomcountry = Math.floor(Math.random() * this.pays.length)
+                this.randompays = this.pays[randomcountry]
+                console.log(this.randompays)
+            }
+    },
 }
 </script> 
