11f1e0c5f926177d9daf4319c09015faa79404cf
ibrahimBOU <85347626+ibrahimBOU@users.noreply.github.com>
2022-10-13 11:56:52 +0200

    start question

--- src/components/MyQuiz.vue
+++ src/components/MyQuiz.vue
@@ -3,7 +3,12 @@
         <div v-if="randompays != null">
             <img :src="randompays.flags.png" alt="">
             <h1>{{ randompays.translations.fra }}</h1>
-            <!-- <input v-if="randompays === "></input> -->
+            <input placeholder="ta reponse" v-model="answer">
+            <h1 v-if="answer === true ">True</h1>
+            <h1 v-else>False</h1>
+            <button @click="valid">Valider la reponse</button>
+        
+
         </div>
 
             
@@ -19,6 +24,7 @@ export default {
         return {
          pays : null ,
          randompays : null,
+         answer : null,
         }
     },
     mounted() {
@@ -38,7 +44,16 @@ export default {
                 let randomcountry = Math.floor(Math.random() * this.pays.length)
                 this.randompays = this.pays[randomcountry]
                 console.log(this.randompays)
+            },
+            checkAnswer(){
+                if(this.valid === this.randompays)
+                this.answer = "true"
+                else{
+                    this.answer = "false"
+                }
+                console.log(this.answer)
             }
+
     },
 }
 </script> 
